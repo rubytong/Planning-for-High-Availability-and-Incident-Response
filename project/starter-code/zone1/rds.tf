584ed5c6ba4b521e3ff930df8c700a36ae6cbb09
ngoctth <hongngoc219.90@gmail.com>
2022-08-06 12:01:03 +0700

    revert HA to fix Granfana Dashboard

--- project/starter-code/zone1/rds.tf
+++ project/starter-code/zone1/rds.tf
@@ -5,7 +5,7 @@ vpc_id = module.vpc.vpc_id
 azs = ["us-east-2a","us-east-2b","us-east-2c"]
 }
 
-module "project_rds_s" {
+/*module "project_rds_s" {
 source                  = "./modules/rds-s"
 primary_db_cluster_arn  = module.project_rds_p.db_cluster_arn
 providers = {
@@ -14,4 +14,4 @@ providers = {
 private_subnet_ids = module.vpc_west.private_subnet_ids
 vpc_id = module.vpc_west.vpc_id
 azs = ["us-west-1a","us-west-1b"]
-}
+}*/
